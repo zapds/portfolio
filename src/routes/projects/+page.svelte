1ffee2dd44a86cde37d7dfa580ce66c00c38d9c9
Zapd0s <71247896+Zapd0s@users.noreply.github.com>
2024-04-26 20:31:34 +0530

    improve responsiveness

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,5 +1,5 @@
-<div class="">
-    <h1 class="text-4xl font-mono text-center font-semibold">
+<div class="pt-4">
+    <h1 class="text-2xl font-mono text-center font-semibold">
         Coming Soon
     </h1>
 </div>
