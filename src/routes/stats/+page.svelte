0a0f03c276a057d739c32fb391b99fd1da555e6a
Soham <nikk.git@gmail.com>
2024-06-20 19:59:44 +0530

    Update +page.svelte

--- src/routes/stats/+page.svelte
+++ src/routes/stats/+page.svelte
@@ -3,6 +3,6 @@
 
 <div class="px-1 flex flex-col items-stretch gap-8">
     <h2 class="pt-4 font-mono font-semibold text-2xl text-center">GitHub Stats</h2>
-    <img alt="github-stats" class="h-48" src="https://github-readme-stats-NotThERealNik.vercel.app/api?username=NotThERealNik&show_icons=true&theme=transparent&text_color=00dfd8&title_color=00dfd8&text_bold=false&include_all_commits=true">
-    <img alt="github-stats" class="h-48" src="https://github-readme-stats-NotThERealNik.vercel.app/api/top-langs/?username=NotThERealNik&show_icons=true&theme=transparent&layout=donut&text_color=00dfd8&title_color=00dfd8">
+    <img alt="github-stats" class="h-48" src="https://github-readme-stats.vercel.app/api?username=NotThERealNik&show_icons=true&theme=transparent&text_color=00dfd8&title_color=00dfd8&text_bold=false&include_all_commits=true">
+    <img alt="github-stats" class="h-48" src="https://github-readme-stats.vercel.app/api/top-langs/?username=NotThERealNik&show_icons=true&theme=transparent&layout=donut&text_color=00dfd8&title_color=00dfd8">
 </div>
